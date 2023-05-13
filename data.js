b3e2b7abec61946c7ba65d88b9a000e5ff20b572
wcartaya <wcartaya@dlocal.com>
2023-05-13 12:08:57 -0300

    initial

---
+++ data.js
@@ -0,0 +1,264 @@
+data = [
+  {
+    "id": 1,
+    "name": "Carrera Maya",
+    "city": "Buenos Aires",
+    "ubicacion": "Palermo",
+    "date": "25/05/2023",
+    "hour": "08:00",
+    "distancias": [
+      10,
+      3
+    ],
+    "costo": [
+      4800,
+      4000
+    ],
+    "url": "./Imagenes eventos Carreras/CarreraMaya.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Run Happy, 10K Brooks",
+    "city": "Buenos Aires",
+    "ubicacion": "Av.Figueroa Alcorta y Monroe",
+    "date": "4/06/2023",
+    "hour": "08:30",
+    "distancias": [
+      10,
+      5
+    ],
+    "costo": [
+      4999,
+      3999
+    ],
+    "url": "./Imagenes eventos Carreras/RunHappy.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Media Maraton Ciudad De La Plata",
+    "city": "La Plata",
+    "ubicacion": "Plaza Moreno La Plata",
+    "date": "11/06/2023",
+    "hour": "08:00",
+    "distancias": [
+      21,
+      10
+    ],
+    "costo": [
+      4999,
+      3999
+    ],
+    "url": "./Imagenes eventos Carreras/MadiaMaratonLaPlata.jpg"
+  },
+  {
+    "id": 4,
+    "name": "10K Rexona",
+    "city": "Buenos Aires",
+    "ubicacion": "Alfonsin y Laprida, Vicente Lopez",
+    "date": "17/06/2023",
+    "hour": "17:00",
+    "distancias": [
+      10,
+      3
+    ],
+    "costo": [
+      4500,
+      4500
+    ],
+    "url": "./Imagenes eventos Carreras/Rexona.jpg"
+  },
+  {
+    "id": 5,
+    "name": "30k Buenos aires",
+    "city": "Buenos Aires",
+    "ubicacion": "Autodromo Oscar y Juan Galvez",
+    "date": "02/07/2023",
+    "hour": "7:45 a 8:30",
+    "distancias": [
+      30
+    ],
+    "costo": [
+      6999
+    ],
+    "url": "./Imagenes eventos Carreras/30kBuenosAires.jpg"
+  },
+  {
+    "id": 6,
+    "name": "Maraton De San Juan",
+    "city": "San Juan",
+    "ubicacion": "Punta Negra San Juan",
+    "date": "23/07/2023",
+    "hour": "7:00",
+    "distancias": [
+      42,
+      21,
+      10,
+      5
+    ],
+    "costo": [
+      8200,
+      7200,
+      6000,
+      5200
+    ],
+    "url": "./Imagenes eventos Carreras/MaratonInternacionalDeSanJuan.jpg"
+  },
+  {
+    "id": 7,
+    "name": "15k De Rosario",
+    "city": "Rosario",
+    "ubicacion": "Monumento Nacional a La Bandera",
+    "date": "23/07/2023",
+    "hour": "7:45 a 8:30",
+    "distancias": [
+      15
+    ],
+    "costo": [
+      5500
+    ],
+    "url": "./Imagenes eventos Carreras/15kRosario.jpg"
+  },
+  {
+    "id": 8,
+    "name": "15k Buenos Aires",
+    "city": "Buenos Aires",
+    "ubicacion": "Av Figueroa Alcorta & Av Dorrego",
+    "date": "06/08/2023",
+    "hour": "7:15 a 8:00",
+    "distancias": [
+      15
+    ],
+    "costo": [
+      5799
+    ],
+    "url": "./Imagenes eventos Carreras/15kBuenosAires.jpg"
+  },
+  {
+    "id": 9,
+    "name": "5k Mc Donalds",
+    "city": "Rosario",
+    "ubicacion": "Av. Dante Alighieri y Blvd.Oroño",
+    "date": "17/09/2023",
+    "hour": "9:00",
+    "distancias": [
+      5
+    ],
+    "costo": [],
+    "url": "./Imagenes eventos Carreras/MujeresEnMarchaRosario.jpg"
+  },
+  {
+    "id": 10,
+    "name": "Trail Tandil",
+    "city": "Tandil",
+    "ubicacion": "Parque Del Lago, a las orillas del Lago Del Fuerte, altura curvón de la Av Saabedra Lamas",
+    "date": "17/09/2023",
+    "hour": "10:00",
+    "distancias": [
+      {
+        "distancia": 5,
+        "costo": 13000
+      },
+      {
+        "distancia": 10,
+        "costo": 11000
+      }
+    ],
+    "url": "./Imagenes eventos Carreras/trailTandil.jpg"
+  },
+  {
+    "id": 11,
+    "name": "Maraton De Buenos Aires",
+    "city": "Buenos Aires",
+    "ubicacion": "Av Figueroa y Monroe",
+    "date": "24/09/2023",
+    "hour": "7:00",
+    "distancias": [
+      42
+    ],
+    "costo": [
+      9700
+    ],
+    "url": "./Imagenes eventos Carreras/MaratonBuenosAires.jpg"
+  },
+  {
+    "id": 12,
+    "name": "15k San Juan",
+    "city": "San Juan",
+    "ubicacion": "Autodromo de Villicum San Juan",
+    "date": "1/10/2023",
+    "hour": "7:15 a 8:00",
+    "distancias": [
+      15,
+      5
+    ],
+    "costo": [
+      5500,
+      4800
+    ],
+    "url": "./Imagenes eventos Carreras/CarreraMaya15kSanJuan.jpg"
+  },
+  {
+    "id": 13,
+    "name": "Medio Maraton Comodoro Rivadavia",
+    "city": "Comodoro",
+    "ubicacion": "Av Costanera Comodoro Rivadavia",
+    "date": "8/10/2023",
+    "hour": "7:15 a 8:00",
+    "distancias": [
+      21,
+      10,
+      5
+    ],
+    "costo": [
+      5499,
+      4499,
+      3699
+    ],
+    "url": "./Imagenes eventos Carreras/MediaMaratonComodoro.jpg"
+  },
+  {
+    "id": 14,
+    "name": "5k Mc Donalds",
+    "city": "Buenos Aires",
+    "ubicacion": "Vicente Lopez",
+    "date": "14/10/2023",
+    "hour": "18:00",
+    "distancias": [
+      5
+    ],
+    "costo": [],
+    "url": "./Imagenes eventos Carreras/MujeresEnMarcha.jpg"
+  },
+  {
+    "id": 15,
+    "name": "15k Mar Del plata Open Sports",
+    "city": "Mar Del Plata",
+    "ubicacion": "Playa Varese",
+    "date": "15/10/2023",
+    "hour": "8:00",
+    "distancias": [
+      15
+    ],
+    "costo": [],
+    "url": "./Imagenes eventos Carreras/marDelPlata.jpg"
+  },
+  {
+    "id": 16,
+    "name": "Media Maraton Mar Del Plata",
+    "city": "Mar Del Plata",
+    "ubicacion": "Av Patricia Peralta Ramos, Frente a la Plaza Almirante Brown",
+    "date": "12/11/2023",
+    "hour": "6:15 a 7:00",
+    "distancias": [
+      21,
+      10,
+      5
+    ],
+    "costo": [
+      5999,
+      4999,
+      4299
+    ],
+    "url": "./Imagenes eventos Carreras/MediaMaratonMarDelPlata.jpg"
+  }
+]
